1f758a17c57c80c0b5a4b309aaf34d0c6afe77b8
Amit Cohen <amitcohen2000@gmail.com>
2026-03-02 00:03:58 +0200

    delete copy angular project

--- nesstest.client - Copy/src/app/Enum/generalEnums.enum.ts
+++
@@ -1,1 +0,0 @@
-export enum ErrorTypeEnum { VALID = 1, INVALID = 2, INVALID_MIN_MAX_DATE = 3 }
--- nesstest.client - Copy/src/app/components/shared/header.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'header-component',
-  standalone: false,
-  templateUrl: 'header.html'
-})
-export class Header { }
